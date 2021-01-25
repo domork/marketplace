5737f9e2b4c6de271610a44eabcf48090d05cf54
Dmytro Kondrashov <e01641210@student.tuwien.ac.at>
2021-01-25 13:03:02 +0100

    first commit

---
+++ backend/src/test/java/com/domork/demo/PetProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.domork.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
